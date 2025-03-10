e4c89e7a1a69ea03564510078f7921becc7d5e14
Howard Liu <146787763+liuhoward0@users.noreply.github.com>
2025-03-10 03:45:12 -0700

    Initial Commit

---
+++ lab1/DemoMulXYAdd100.s
@@ -0,0 +1,50 @@
+	.file	"add.c"
+	.section	.rodata
+.LC0:
+	.string	"x=%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$20, %esp
+// core code starts here, x=21; y=5; x=x*y+100
+	movl	$21, x
+	movl	$5, y
+	movl	x, %eax
+	imull   y, %eax
+	addl	$100, %eax
+	movl	%eax, x
+// call printf with 2 parameters: x and "x=%d\n"
+	subl	$8, %esp
+	pushl	x
+	pushl	$.LC0
+	call	printf
+	addl	$16, %esp
+	movl	$0, %eax
+	movl	-4(%ebp), %ecx
+	.cfi_def_cfa 1, 0
+	leave
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
+        .data
+x:      .quad   0
+y:      .quad   0
+
