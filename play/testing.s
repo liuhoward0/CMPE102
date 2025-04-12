763dcb7967a509cddf859b81e7a4382d9859d026
Howard <146787763+liuhoward0@users.noreply.github.com>
2025-04-12 09:04:08 +0000

    fucked around

--- play/testing.s
+++ play/testing.s
@@ -5,16 +5,19 @@
 main:
 .LFB0:
 	.cfi_startproc
+	endbr32
 	leal	4(%esp), %ecx
 	.cfi_def_cfa 1, 0
 	andl	$-16, %esp
 	pushl	-4(%ecx)
 	pushl	%ebp
-	.cfi_escape 0x10,0x5,0x2,0x75,0
 	movl	%esp, %ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
 	pushl	%ecx
 	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
 	subl	$4, %esp
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
 	subl	$8, %esp
 	pushl	$2
 	pushl	$1
@@ -36,11 +39,14 @@ main:
 foo:
 .LFB1:
 	.cfi_startproc
+	endbr32
 	pushl	%ebp
 	.cfi_def_cfa_offset 8
 	.cfi_offset 5, -8
 	movl	%esp, %ebp
 	.cfi_def_cfa_register 5
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
 	movl	$3, 8(%ebp)
 	movl	$4, 12(%ebp)
 	nop
@@ -51,5 +57,32 @@ foo:
 	.cfi_endproc
 .LFE1:
 	.size	foo, .-foo
-	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB2:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE2:
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 4
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 4
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 4
+4:
